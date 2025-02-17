9c84acdf80d77fef4508b8e9a4d0ce6b0195e19b
Anton Papazov <134053962+papazoff06@users.noreply.github.com>
2025-02-17 12:29:53 +0200

    Delete furry_funnies directory

--- furry_funnies/furry_funnies/common/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-
-from furry_funnies.common.views import index, dashboard
-
-urlpatterns = [
-path('', index, name='index'),
-    path('dashboard/', dashboard, name='dashboard'),
-]
--- furry_funnies/furry_funnies/utils.py
+++
@@ -1,9 +0,0 @@
-from furry_funnies.authors.models import Author
-from furry_funnies.posts.models import Post
-
-
-def get_author_obj():
-    return Author.objects.first()
-
-def get_post_obj():
-    return Post.objects.all()
